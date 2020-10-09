ad75945f74ec81212499c74b73ffeb6260bbd0b7
LyricDuda <Lyricsduda@gmail.com>
2020-10-09 17:16:11 -0400

    Added require's for index and api routes

--- public/server.js
+++ public/server.js
@@ -10,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/assets", express.static("./assets"));
 
+// Requier's for the index and ip routes
+require("./routes/indexRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 // app listen and function
 app.listen(PORT, function() {
