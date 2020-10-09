1fe51d0e45de62e1e6f1759b0b625360a9abfd16
LyricDuda <Lyricsduda@gmail.com>
2020-10-09 17:58:57 -0400

    Added .get infromation to the function

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -4,4 +4,14 @@ var data = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
 
 module.exports = fuction(app) {
 
+    // Receive JSON data from notes
+    app.get("/api/notes", function(req, res){
+        res.json(data);
+    });
+
+    // Shows the JSON data when you go to the url localhost:port/api/notes/:id
+    app.get("/api/notes/:id", function(req, res){
+        res.json(Data[Number(req.params.id)]);
+    });
+
 }
